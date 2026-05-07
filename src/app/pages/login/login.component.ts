d023662a85573a2eb99fa899daaf29c4abfcf459
Camilo <ernesto.fagundez@estudiantes.utec.edu.uy>
2026-05-07 19:53:37 -0300

    .

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',

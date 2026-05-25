21b1e82a29b973204bf2f207a20db520c4d33b75
Dekyim <adrv97@gmail.com>
2026-05-25 14:08:34 -0300

    Se quitaron variables de entorno

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://localhost:8081/sistemacentrocrecer/api/v1',
-  cloudinaryCloudName: 'TU_CLOUD_NAME',
-  cloudinaryUploadPreset: 'TU_UPLOAD_PRESET'
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8081/sistemacentrocrecer/api/v1',
-  cloudinaryCloudName: 'dhm5shtz6',
-  cloudinaryUploadPreset: 'centro-crecer'
-};
